512c4de6340169ecd9ea9aec86da7fb3fe74f45b
Kwiky93 <sei93@yandex.ru>
2024-05-17 01:04:17 +0300

    Correcting url images

--- stores/pages.ts
+++ stores/pages.ts
@@ -6,34 +6,34 @@ export const usePages = defineStore("pages", {
       [
         {
           color: "green",
-          pic: "/pics/3.jpg",
+          pic: "/test-parallax/pics/3.jpg",
           link: "/test3",
         },
         {
           color: "green",
-          pic: "/pics/4.jpg",
+          pic: "/test-parallax/pics/4.jpg",
           link: "/test4",
         },
         {
           color: "green",
-          pic: "/pics/5.jpg",
+          pic: "/test-parallax/pics/5.jpg",
           link: "/test5",
         },
       ],
       [
         {
           color: "red",
-          pic: "/pics/1.jpg",
+          pic: "/test-parallax/pics/1.jpg",
           link: "/test",
         },
         {
           color: "purple",
-          pic: "/pics/7.jpg",
+          pic: "/test-parallax/pics/7.jpg",
           link: "/test7",
         },
         {
           color: "white",
-          pic: "/pics/8.jpg",
+          pic: "/test-parallax/pics/8.jpg",
           link: "/test8",
         },
       ],
@@ -41,25 +41,25 @@ export const usePages = defineStore("pages", {
       [
         {
           color: "yellow",
-          pic: "/pics/6.jpg",
+          pic: "/test-parallax/pics/6.jpg",
           link: "/test6",
         },
         {
           color: "blue",
-          pic: "/pics/2.jpeg",
+          pic: "/test-parallax/pics/2.jpeg",
           link: "/test2",
         },
 
         {
           color: "red",
-          pic: "/pics/9.png",
+          pic: "/test-parallax/pics/9.png",
           link: "/test9",
         },
       ],
       [
         {
           color: "red",
-          pic: "/pics/10.jpg",
+          pic: "/test-parallax/pics/10.jpg",
           link: "/test10",
         },
       ],
