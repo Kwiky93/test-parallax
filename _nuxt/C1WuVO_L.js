44f78f3a452860cb0255299642313e41cc088b7c
Kwiky93 <sei93@yandex.ru>
2024-05-17 00:56:57 +0300

    Updates

---
+++ _nuxt/C1WuVO_L.js
@@ -0,0 +1,1 @@
+import{L as r,t as o,v as c,y as t,M as e,N as n}from"./DvxKPHK1.js";import{u as i}from"./D1l5K0sY.js";const d={__name:"[id]",setup(p){const s=r(),a=i().getPicsByLink(s.params.id);return(l,m)=>(o(),c("div",{ref:"page",class:"page",style:n({backgroundImage:`url(${e(a).pic})`})}," PAge "+t(e(s).params.id)+" "+t(e(a)),5))}};export{d as default};
